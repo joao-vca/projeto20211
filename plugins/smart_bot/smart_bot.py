59eff96341a0811e489c1305ff8b0c322224eb0f
joao-vca <joao.vca@aluno.ifsc.edu.br>
2021-08-30 23:48:02 +0000

    Chatbot funcionando V1

---
+++ plugins/smart_bot/smart_bot.py
@@ -0,0 +1,66 @@
+from errbot import BotPlugin, botcmd, botmatch
+
+nome = None
+modelocam = None
+problemas = None
+
+class Atendimento(BotPlugin):
+    @botcmd
+    def ajudacamera(self, msg, args):
+        """
+        Começa o atendimento perguntando o nome do cliente.
+        """
+        yield "Olá! Seja bem-vindo ao canal de suporte técnico da Intelbras no Discord!"
+        yield "Eu me chamo Smart, o robô de suporte das câmeras Mibo."
+        yield "Para começar, comece dizendo o seu nome para começar o nosso atendimento:"
+
+    @botmatch(r'.*$', flow_only=True)
+    def nomear(self, msg, match):
+        """
+        Começa o atendimento perguntando o nome do cliente e salva.
+        """
+        global nome
+        nome = match.string.capitalize()
+        yield "Olá, " + nome + "! Vamos começar o nosso atendimento."
+
+    @botmatch(r'.*$', flow_Only=True)
+    def modelocam(self, msg, match):
+        """
+        Pergunta o modelo da câmera e salva.
+        """
+        global modelo
+        modelo = match.string.capitalize()
+        yield "Para começar, comece dizendo o modelo da sua câmera:"
+        yield "iM3"
+        yield "iM4"
+        yield "iM5 S"
+    
+        if modelo == "iM3":
+            yield "OK, sua câmera é uma iM3, iM3 Duo, iM3 Black ou iM3 c/micro-SD."
+        
+        if modelo == "iM4":
+            yield "OK, sua câmera é uma iM4 ou iM4 c/micro-SD."
+
+        if modelo == "iM5 S":
+            yield "OK, sua câmera é uma iM5 S ou iM5 S c/micro-SD."
+    
+    @botmatch(r'.*$', flow_Only=True)
+    def problemas(self, msg, match):
+        """
+        Qual problema o cliente está enfrentando?
+        """
+        global problema
+        problema = match.string.capitalize()
+        yield "Certo. Qual problema você está enfrentando?"
+        yield "(1) Não conecta"
+        yield "(2) Desvincular câmera"
+        yield "(3) Ajuda com compartilhamento"
+
+        if problema == "(1) Não conecta":
+            yield "OK"
+        
+        if problema == "(2) Desvincular câmera":
+            yield "OK"
+
+        if problema == "(3) Ajuda com compartilhamento":
+            yield "OK"
---
+++ plugins/smart_bot/smart_flow.py
@@ -0,0 +1,13 @@
+from errbot import botflow, FlowRoot, BotFlow
+
+
+class AtendimentoFlow(BotFlow):
+    """
+    Flow para suporte técnico.
+    """
+    @botflow
+    def passoapasso(self, flow: FlowRoot):
+        first_step = flow.connect('começar', auto_trigger=True)
+        second_step = first_step.connect('nomear')
+        third_step = second_step.connect('modelocam')
+        fourth_step = third_step.connect('problemas')
