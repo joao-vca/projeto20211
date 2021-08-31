df07788a1a00b2f41978b065493450d93095f85f
joao-vca <joao.vca@aluno.ifsc.edu.br>
2021-08-31 23:37:59 +0000

    Atualização 1

--- plugins/smart_bot/smart_flow.py
+++ plugins/smart_bot/smart_flow.py
@@ -1,13 +1,12 @@
 from errbot import botflow, FlowRoot, BotFlow
 
-
 class AtendimentoFlow(BotFlow):
     """
     Flow para suporte técnico.
     """
     @botflow
     def passoapasso(self, flow: FlowRoot):
-        first_step = flow.connect('ajudacamera', auto_trigger=True)
+        first_step = flow.connect('ajudacamera')
         second_step = first_step.connect('nomear')
         third_step = second_step.connect('modelocam')
         fourth_step = third_step.connect('problemas')
